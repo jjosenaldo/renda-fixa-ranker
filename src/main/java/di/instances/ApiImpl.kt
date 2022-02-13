dfef2c6ac34ba063b89c832afbf7047a4729a18a
Josenaldo Júnior <josenaldo.asj@gmail.com>
2022-02-12 22:18:47 -0300

    Fix the value of IPCA

--- src/main/java/di/instances/ApiImpl.kt
+++ src/main/java/di/instances/ApiImpl.kt
@@ -6,7 +6,7 @@ import api.Indice
 internal class ApiImpl : Api {
     override fun getValorIndice(indice: Indice): Double {
         return when (indice) {
-            Indice.Ipca -> 0.7
+            Indice.Ipca -> 0.07
             Indice.Cdi -> 0.1
         }
     }
